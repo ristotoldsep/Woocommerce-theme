c37c0f9e5d504a90da36456e9a255442c07ac79a
Risto Tõldsep <r.toldsep@gmail.com>
2022-09-05 21:36:26 +0300

    Added updates

--- wp-content/themes/fancy-lab/functions.php
+++ wp-content/themes/fancy-lab/functions.php
@@ -51,47 +51,9 @@ function fancy_lab_config() {
 
 add_action('after_setup_theme','fancy_lab_config', 0 );
 
-// add_action('after_setup_theme', 'woocommerce_support');
-// function woocommerce_support()
-// {
-//     add_theme_support('woocommerce');
-// }
-
-add_action('woocommerce_before_main_content','fancy_lab_open_container_row', 5);
-
-function fancy_lab_open_container_row() {
-    echo '<div class="container shop-content"><div class="row">';
+//Only load these if WC is activated
+if ( class_exists( 'WooCommerce' )) {
+    require get_template_directory() . '/inc/wc-modifications.php';
 }
-
-add_action('woocommerce_after_main_content', 'fancy_lab_close_container_row', 5);
-function fancy_lab_close_container_row()
-{
-    echo '</div></div>';
-}
-
-add_action('woocommerce_before_main_content','fancy_lab_add_sidebar_tags', 6);
-function fancy_lab_add_sidebar_tags() {
-    echo '<div class="sidebar-shop col-lg-3 col-md-4 order-2 order-md-1">';
-}
-
-remove_action('woocommerce_sidebar', 'woocommerce_get_sidebar');
-add_action('woocommerce_before_main_content', 'woocommerce_get_sidebar', 7);
-
-add_action('woocommerce_before_main_content', 'fancy_lab_close_sidebar_tags', 8);
-function fancy_lab_close_sidebar_tags()
-{
-    echo '</div>';
-}
-
-add_action('woocommerce_before_main_content', 'fancy_lab_add_shop_tags', 9);
-function fancy_lab_add_shop_tags() {
-    echo '<div class="col-lg-9 col-md-8 order-1 order-md-2">';
-}
-
-add_action('woocommerce_after_main_content', 'fancy_lab_close_shop_tags', 4);
-function fancy_lab_close_shop_tags() {
-    echo '</div>';
-}
-
 
 ?>
