ddfd81b3ff229d23be44522cc9b27f037b54e75b
Risto Tõldsep <r.toldsep@gmail.com>
2022-09-09 17:42:24 +0300

    Updated Blog

--- wp-content/themes/fancy-lab/page.php
+++ wp-content/themes/fancy-lab/page.php
@@ -1,43 +1,43 @@
 <?php
+
 /**
  * The template for displaying all pages
  *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  *
- * @package Fancy Lab By RIX
-
+ * @package Fancy Lab By Rix
  */
 
 get_header();
-
 ?>
-
 <div class="content-area">
     <main>
         <div class="container">
             <div class="row">
                 <?php
-                //If there are any posts
-                if (have_posts()) :
-
-                    //Load posts loop
-                    while (have_posts()) : the_post();
+                // Load posts loop
+                while (have_posts()) : the_post();
                 ?>
-                        <article class="col">
-                            <h1><?php the_title(); ?></h1>
-                            <div><?php the_content(); ?></div>
-                        </article>
-                    <?php
-                    endwhile;
-                else :
-                    ?>
-                    <p>Nothing to display.</p>
+                    <article class="col">
+                        <h1><?php the_title(); ?></h1>
+                        <div><?php the_content(); ?></div>
+                        <?php
+                        // If comments are open or we have at least one comment, load up the comment template.
+                        if (comments_open() || get_comments_number()) :
+                            comments_template();
+                        endif;
+                        ?>
+                    </article>
                 <?php
-                endif;
+                endwhile;
                 ?>
             </div>
         </div>
-       
     </main>
 </div>
 <?php get_footer(); ?>
