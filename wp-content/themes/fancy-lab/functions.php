2948a11aa7209f862c66de71d8e285cf461e3d67
Risto Tõldsep <r.toldsep@gmail.com>
2022-09-13 01:38:43 +0300

    Added error404.php template

--- wp-content/themes/fancy-lab/functions.php
+++ wp-content/themes/fancy-lab/functions.php
@@ -89,6 +89,9 @@ function fancy_lab_config()
     if (!isset($content_width)) {
         $content_width = 600;
     }
+
+    //Important for page title and SEO!
+    add_theme_support( 'title-tag' );
 }
 
 add_action('after_setup_theme', 'fancy_lab_config', 0);
